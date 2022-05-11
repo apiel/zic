c467a6c51a92cd2fa4cb772fa0daed5881fd4b84
Alexandre Piel <Alexandre.Piel@zooplus.com>
2022-05-11 22:09:51 +0200

    fix wavetables

--- wavetables/tools/src/wavetable.ts
+++ wavetables/tools/src/wavetable.ts
@@ -2,8 +2,19 @@ import { writeFileSync } from 'fs';
 
 export const WAVETABLE_SIZE = 2048;
 
-export function save(table: number[], name: string) {
+export function save(table: number[], name: string, float = false) {
     const capName = name[0].toUpperCase() + name.slice(1);
+
+    const data = float ? `
+    float _table[${WAVETABLE_SIZE}] = {${table
+        .map((val) => (val === 0 || val === 1 || val === -1 ? `${val}.0f` : `${val}f`))
+        .join(', ')},};
+    `: `
+    int16_t _table[${WAVETABLE_SIZE}] = {${table
+        .map((val) => Math.round(val * 10000))
+        .join(', ')},};
+    `;
+
     const content = `
 #ifndef WAVETABLE_${name.toUpperCase()}_H_
 #define WAVETABLE_${name.toUpperCase()}_H_
@@ -17,9 +28,7 @@ public:
     {
     }
 protected:
-    float _table[${WAVETABLE_SIZE}] = {${table
-        .map((val) => (val === 0 || val === 1 || val === -1 ? `${val}.0f` : `${val}f`))
-        .join(', ')},};
+${data}
 };
 
 Wavetable_${capName} wavetable_${capName};
