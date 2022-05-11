adcbac25145c27e98f967a01d7d4370b7fefafd6
Alexandre Piel <Alexandre.Piel@zooplus.com>
2022-05-11 20:29:49 +0200

    first commit

---
+++ zic_mod_asr.h
@@ -0,0 +1,15 @@
+#ifndef ZIC_MOD_ASR_H_
+#define ZIC_MOD_ASR_H_
+
+#include "zic_mod_asrBase.h"
+
+// FIXME still make a cutting noise
+/**
+ * @brief ASR envelop modulation
+ * 
+ */
+class Zic_Mod_Asr : public Zic_Mod_AsrBase<float>
+{
+};
+
+#endif
