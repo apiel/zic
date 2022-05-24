cba42b441741d3adb7959d0084eb1effc13de859
Alexandre Piel <Alexandre.Piel@zooplus.com>
2022-05-24 21:06:19 +0200

    tempo base on sample rate

--- zic_seq_tempo.h
+++ zic_seq_tempo.h
@@ -1,14 +1,17 @@
 #ifndef ZIC_SEQ_TEMPO_H_
 #define ZIC_SEQ_TEMPO_H_
 
+#include "zic_def.h"
+
 // 4 ♬ ♬ step per beat
 template <uint8_t STEP_PER_BEAT = 4>
-class Zic_Seq_Tempo
-{
+class Zic_Seq_Tempo {
 protected:
     unsigned long previous = 0;
     uint16_t tempo;
     uint16_t bpm;
+    uint16_t sampleCount = 0;
+    unsigned long time;
 
 public:
     Zic_Seq_Tempo()
@@ -22,12 +25,35 @@ public:
         tempo = 60000 / (bpm * STEP_PER_BEAT);
     }
 
+    /**
+     * @brief Tempo base on given time
+     *
+     * @param now
+     * @return true
+     * @return false
+     */
     bool next(unsigned long now)
     {
-        if ((now - previous) >= tempo)
-        {
+        if ((now - previous) >= tempo) {
             previous = now;
             return true;
+        }
+        return false;
+    }
+
+    /**
+     * @brief Tempo base on sample rate
+     *
+     * @return true
+     * @return false
+     */
+    bool next()
+    {
+        sampleCount++;
+        if (sampleCount > SAMPLE_PER_MS) {
+            sampleCount = 0;
+            time++;
+            return next(time);
         }
         return false;
     }
--- zic_wave_base.h
+++ zic_wave_base.h
@@ -3,30 +3,7 @@
 
 #include <math.h>
 
-#ifndef SAMPLE_RATE
-#define SAMPLE_RATE 44100
-#endif
-
-// sample frequency = 44100 Hz, so one second equals 44100 samples.
-#ifndef DELTA_TIME
-#define DELTA_TIME 1.0f / SAMPLE_RATE
-#endif
-
-#ifndef SAMPLE_PER_MS
-#define SAMPLE_PER_MS SAMPLE_RATE / 1000 // 44.1
-#endif
-
-#ifndef MAX_FREQUENCY
-#define MAX_FREQUENCY SAMPLE_RATE * 0.25
-#endif
-
-#ifndef between
-#define between(x, a, b) (((a) <= (x)) && ((x) <= (b)))
-#endif
-
-#ifndef range
-#define range(x, _min, _max) ((x) < (_max) ? ((x) > (_min) ? (x) : (_min)) : (_max))
-#endif
+#include "zic_def.h"
 
 #ifndef FREQ_MULT
 #define FREQ_MULT 100
