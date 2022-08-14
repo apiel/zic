511e79646ed9dce746a4d66489e71376f6db7b89
Alexandre Piel <Alexandre.Piel@zooplus.com>
2022-08-14 23:04:37 +0200

    wav file

--- zic_file_audio.h
+++ zic_file_audio.h
@@ -8,19 +8,12 @@
 class Zic_File_Audio : public Zic_File {
 protected:
     typedef struct WavHeader {
-        uint32_t ChunkID;
-        uint32_t ChunkSize;
-        uint32_t Format;
-        uint32_t Subchunk1ID;
-        uint32_t Subchunk1Size;
         uint16_t AudioFormat;
         uint16_t NumChannels;
         uint32_t SampleRate;
         uint32_t ByteRate;
         uint16_t BlockAlign;
         uint16_t BitsPerSample;
-        uint32_t Subchunk2ID;
-        uint32_t Subchunk2Size;
     } WavHeader;
 
     WavHeader header;
@@ -45,37 +38,48 @@ public:
             return NULL;
         }
 
-        Zic_File::read((uint8_t*)&header, sizeof(WavHeader));
-
-        // skip over any other chunks before the "data" chunk
-        bool additionalHeaderDataPresent = false;
-        while (header.Subchunk2ID != 1635017060) {
-            seekFromCurrent(4);
-            Zic_File::read((uint8_t*)&header.Subchunk2ID, 4);
-            additionalHeaderDataPresent = true;
+        bool dataAvailable = false;
+        uint32_t chunkID;
+        uint32_t chunkSize;
+        char data[512];
+        // while readfile
+        while (Zic_File::read(&chunkID, 4)) {
+            // printf("(%d) %c%c%c%c\n", chunkID, (char)(chunkID & 0xFF), (char)((chunkID >> 8) & 0xFF), (char)((chunkID >> 16) & 0xFF), (char)((chunkID >> 24) & 0xFF));
+            // 1414744396 -> LIST
+            // 70468681 -> ID3
+            // 1179011410 -> RIFF
+            // 1163280727 -> WAVE
+            // 544501094 -> fmt
+            // Could check that first chunk is RIFF and 3th one is WAVE
+            if (chunkID == 1414349641) { // ICMT
+                Zic_File::read(&chunkSize, 4);
+                Zic_File::read(&data, chunkSize);
+                printf("ICMT Data: %s\n\n", data);
+            } else if (chunkID == 1635017060) { // data
+                Zic_File::read(&chunkSize, 4);
+                start = tell();
+                end = start + chunkSize;
+                seekFromCurrent(chunkSize);
+                dataAvailable = true;
+            } else if (chunkID == 544501094) { // fmt
+                Zic_File::read(&chunkSize, 4);
+                if (Zic_File::read((uint8_t*)&header, sizeof(WavHeader)) != chunkSize) {
+                    printf("Something went wrong reading the fmt chunk\n");
+                    // return NULL; // we cann still continue
+                }
+            }
         }
 
-        if (additionalHeaderDataPresent) {
-            // read the value of Subchunk2Size, the one populated when reading 'WavHeader' structure is wrong
-            Zic_File::read((uint8_t*)&header.Subchunk2Size, 4);
-        }
-
-        // Should we do this?
-        // if (header.BitsPerSample != 16 || header.AudioFormat != 1) {
-        //     file = NULL;
-        //     return NULL;
-        // }
-
-        start = tell();
-        seekFromEnd(0);
-        end = tell();
         restart();
         sampleCount = (end - start) / (header.BitsPerSample); // * header.NumChannels
+
+        // printf("%s: %d channels, %d Hz, %d bits, chunksize %d Subchunk1Size %d DatachunkSize %d\n",
+        //     filename, header.NumChannels, header.SampleRate, header.BitsPerSample, header.chunkSize, header.Subchunk1Size, header.DatachunkSize);
 
         // printf("Audio file %d %d %d start %ld end %ld sampleCount %ld\n",
         //     header.BitsPerSample, header.AudioFormat, header.NumChannels, (long)start, (long)end, (long)sampleCount);
 
-        return file;
+        return dataAvailable ? file : NULL;
     }
 
     void seekToSample(uint64_t pos)
