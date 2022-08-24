5e266d75a04f2d2e1b063ca6d1ad9b6d4eb92e69
Alexandre Piel <Alexandre.Piel@zooplus.com>
2022-08-24 22:19:39 +0200

    wip

--- zic_file_audio.h
+++ zic_file_audio.h
@@ -1,20 +1,20 @@
 #ifndef ZIC_FILE_AUDIO_H_
 #define ZIC_FILE_AUDIO_H_
 
+#include "zic_file_soundfont.h"
 #include "zic_file_wav.h"
 
+// class Zic_File_Audio : public Zic_File_Wav, Zic_File_Soundfont {
 class Zic_File_Audio : public Zic_File_Wav {
 protected:
-
 public:
-
     // Zic_File_Audio()
     // {
     // }
 
     // Zic_File_Audio(const char* filename)
     // {
-    //     open(filename);
+    //     Zic_File_Wav::open(filename);
     // }
 };
 
