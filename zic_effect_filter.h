a17e4b14c21de0084af651e8d881dc3c71957934
Alexandre Piel <Alexandre.Piel@zooplus.com>
2022-10-30 22:29:27 +0100

    filter

--- zic_effect_filter.h
+++ zic_effect_filter.h
@@ -1,34 +1,30 @@
 #ifndef ZIC_EFFECT_FILTER_H_
 #define ZIC_EFFECT_FILTER_H_
 
+#include <math.h>
+
 #include "./zic_def.h"
 
-// http://www.martin-finke.de/blog/articles/audio-plugins-013-filter/
+// https://www.martin-finke.de/articles/audio-plugins-013-filter/
+// https://www.musicdsp.org/en/latest/Filters/29-resonant-filter.html
 
-// TODO rename this to Zic_Effect_Filter
-
-class Zic_Effect_Filter
-{
+class Zic_Effect_Filter {
 protected:
     int16_t buf0 = 0.0;
     int16_t buf1 = 0.0;
     int16_t buf2 = 0.0;
     int16_t buf3 = 0.0;
 
-    float cutoffCalc = 0.2;
-    // cutoff cannot be 1.0 else div by zero
     float feedback;
     void calculateVar()
     {
-        cutoffCalc = cutoff;
-        // cutoffCalc = 1.0f - cutoff * cutoff;
-        feedback = resonance + resonance / (1.0 - cutoffCalc);
-        // Serial.printf("cutoff %.2f calc %.2f res %.2f feedback %.2f\n", cutoff, cutoffCalc, resonance, feedback);
+        // cutoff = 2.0*sin(pi*freq/samplerate);
+        feedback = resonance + resonance / (1.0 - cutoff);
+        // Serial.printf("cutoff %.2f calc %.2f res %.2f feedback %.2f\n", cutoff, cutoff, resonance, feedback);
     }
 
 public:
-    enum FilterMode
-    {
+    enum FilterMode {
         FILTER_MODE_OFF,
         FILTER_MODE_LOWPASS,
         FILTER_MODE_HIGHPASS,
@@ -36,27 +32,28 @@ public:
         FILTER_MODE_COUNT,
     };
 
-    float cutoff = 0.2;
+    // uint16_t frequency = (uint16_t)(SAMPLE_RATE / 2.0);
+    uint16_t frequency = 8000;
+    float cutoff = 0.99;
     float resonance = 0.0;
     FilterMode mode = FILTER_MODE_LOWPASS;
 
     Zic_Effect_Filter()
     {
-        calculateVar();
+        // calculateVar();
+        setFrequency(frequency);
     };
 
     int16_t next(int16_t inputValue)
     {
-        if (mode == FILTER_MODE_OFF || inputValue == 0)
-        {
+        if (mode == FILTER_MODE_OFF || inputValue == 0) {
             return inputValue;
         }
-        buf0 += cutoffCalc * (inputValue - buf0 + feedback * (buf0 - buf1));
-        buf1 += cutoffCalc * (buf0 - buf1);
-        buf2 += cutoffCalc * (buf1 - buf2);
-        buf3 += cutoffCalc * (buf2 - buf3);
-        switch (mode)
-        {
+        buf0 += cutoff * (inputValue - buf0 + feedback * (buf0 - buf1));
+        buf1 += cutoff * (buf0 - buf1);
+        buf2 += cutoff * (buf1 - buf2);
+        buf3 += cutoff * (buf2 - buf3);
+        switch (mode) {
         case FILTER_MODE_LOWPASS:
             return buf3;
         case FILTER_MODE_HIGHPASS:
@@ -68,13 +65,23 @@ public:
         }
     }
 
-    // TODO
-    // on lower freq "value" should be more step, so going expodentially down
-    // at least for LPF, might be the opposite for HPF
+    void setFrequency(uint16_t freq)
+    {
+        frequency = range(freq, 0, 8000);
+        cutoff = 2.0 * sin(M_PI * frequency / SAMPLE_RATE);
+        printf("cutoff %.2f\n", cutoff);
+        set(cutoff, resonance);
+    }
+
     void setCutoff(float _cutoff)
     {
         set(_cutoff, resonance);
     };
+
+    float getFrequencyFromCutoff()
+    {
+        return SAMPLE_RATE * asin(cutoff / 2.0) / M_PI;
+    }
 
     void setResonance(float _res)
     {
@@ -83,6 +90,7 @@ public:
 
     void set(float _cutoff, float _res)
     {
+        // cutoff cannot be 1.0 else div by zero
         cutoff = range(_cutoff, 0.01, 0.99);
         resonance = range(_res, 0.00, 0.99);
         calculateVar();
@@ -98,10 +106,9 @@ public:
         mode = (FilterMode)(value % FILTER_MODE_COUNT);
     }
 
-    const char *getName()
+    const char* getName()
     {
-        switch (mode)
-        {
+        switch (mode) {
         case FILTER_MODE_LOWPASS:
             return "LPF";
         case FILTER_MODE_HIGHPASS:
