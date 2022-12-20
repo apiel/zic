e66ac2272518e48b99d7a4fa02b90e05fd23159c
Alexandre Piel <Alexandre.Piel@zooplus.com>
2022-12-20 17:18:41 +0100

    fileChunks

--- zic_file_audio_libsndfile.h
+++ zic_file_audio_libsndfile.h
@@ -5,14 +5,53 @@
 #include <string.h>
 
 class Zic_File_Audio {
+protected:
+    Zic_File fileChunks;
+
+    void loadCustomChunks(const char* filename)
+    {
+        if (fileChunks.open(filename, "rb") == NULL) {
+            return;
+        }
+
+        uint32_t chunkID;
+        uint32_t chunkSize;
+        while (fileChunks.read(&chunkID, 4)) {
+            // printf("(%d) %c%c%c%c\n", chunkID, (char)(chunkID & 0xFF), (char)((chunkID >> 8) & 0xFF), (char)((chunkID >> 16) & 0xFF), (char)((chunkID >> 24) & 0xFF));
+            // Could check that first chunk is RIFF and 3th one is WAVE
+            // convert "data" 4 char[] to uint32 in order to compare it to chunkID
+            if (chunkID == *(uint32_t*)"data") {
+                fileChunks.read(&chunkSize, 4);
+                fileChunks.seekFromCurrent(chunkSize);
+            } else if (chunkID == *(uint32_t*)"fmt ") {
+                fileChunks.read(&chunkSize, 4);
+                fileChunks.seekFromCurrent(chunkSize);
+            } else if (chunkID == *(uint32_t*)"ZICW") {
+                fileChunks.read(&wavetableCount, 4);
+                // printf("ZICW Data: %d\n", wavetableCount);
+            }
+            // See https://github.com/mtytel/vital/blob/main/src/interface/editor_sections/wavetable_edit_section.cpp#L611
+            // meta_data.set("clm ", "<!>2048 20000000 wavetable (vital.audio)");
+            // vital synth set the chunkID to "clm " and the chunkSize to 2048
+            else if (chunkID == *(uint32_t*)"clm ") {
+                fileChunks.read(&chunkSize, 4);
+                // we could check that string start by "<!>2048"
+                // in vital there 256 wavetable of 2048 samples
+                wavetableCount = 256;
+                // There might be a problem as sample rate is not 44100 but 88200
+                // however so far the result is not good
+                fileChunks.seekFromCurrent(chunkSize);
+            }
+        }
+
+        fileChunks.close();
+    }
+
 public:
     SF_INFO sfinfo;
 
     uint64_t sampleCount = 0;
-    // FIXME
-    // uint32_t wavetableCount = 1;
-    uint32_t wavetableCount = 64;
-    // uint8_t bytesPerSample = sizeof(float);
+    uint32_t wavetableCount = 1;
     SNDFILE* file = NULL;
 
     Zic_File_Audio()
@@ -36,12 +75,15 @@ public:
     {
         close();
 
+        loadCustomChunks(filename);
+
         if (!(file = sf_open(filename, SFM_READ, &sfinfo))) {
             printf("Error: could not open file %s\n", filename);
             return NULL;
         }
         sampleCount = sfinfo.frames;
-        printf("Audio file %s sampleCount %ld\n", filename, (long)sampleCount);
+        // printf("Audio file %s sampleCount %ld\n", filename, (long)sampleCount);
+
         return file;
     }
 
